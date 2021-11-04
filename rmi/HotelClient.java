82a06bb673f67840e2121e129f1e3af0c1c9683b
Filipe Coutinho <filipezarcocoutinho@gmail.com>
2021-11-04 21:20:31 +0000

    nothing

--- rmi/HotelClient.java
+++ rmi/HotelClient.java
@@ -4,7 +4,8 @@ import java.rmi.*;
 public class HotelClient{
     public static void main(String args[]) {
         try {
-            String serverURL = "rmi://" + args[0] + "/HotelServer";
+            //String serverURL = "rmi://" + args[0] + "/HotelServer";
+            String serverURL = "rmi://172.18.100.164/HotelServer";
             HotelServerIntf HotelServerIntf = (HotelServerIntf)Naming.lookup(serverURL);
 
             String res = HotelServerIntf.ping();
